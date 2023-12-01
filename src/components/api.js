951cba63a2977d16d34943a046c6848da2aee3e6
Raymond Huang <raymondhuang615@yahoo.com>
2023-12-01 00:01:24 -0800

    Working on realEstate Api

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,21 @@
+import axios from 'axios';
+
 export const fetchAllData = async () => {
+
+    const options = {
+        method: 'GET',
+        url: 'https://realtor16.p.rapidapi.com/property',
+        params: {
+            property_id: '1497548641'
+        },
+        headers: {
+            'X-RapidAPI-Key': 'c77ac38ea7mshc3153da3be08b85p1fa043jsncb05780c8367',
+            'X-RapidAPI-Host': 'realtor16.p.rapidapi.com'
+        }
+    };
+
     try {
-        const realEstateData = axios.get('');
+        const realEstateData = await axios.request(options);
         return realEstateData;
     } catch (error) {
         console.log(error)
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchAllData } from './api';
 
 const Homepage = () => {
+
+    const [allData, setAllData] = useState([]);
+
+    useEffect(() => {
+        const getAllData = async () => {
+            const realEstate = await fetchAllData();
+            setAllData(realEstate.data);
+            console.log(realEstate.data)
+        };
+
+        getAllData();
+    }, [])
+
+
     return (
-        <div>
+        <div className='container'>
             <div className='text-custom-whiteColor text-center font-Lato not-italic leading normal m-60'>
                 <h1 className='realEstate-Title text-[28px] md:text-[45px] lg:text-[48px]'>Real Estate App</h1>
                 <h2 className='realEstate-Subtitle text-[14px] md:text-[18px] lg:text-[21px]'>Open the door to your <span className='text-custom-greenColor'>dream home</span> with us </h2>
@@ -61,27 +76,43 @@ const Homepage = () => {
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
-                <div class="card" style={{ width: '21rem' }}>
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp" class="card-img-top" alt="Sunset Over the Sea" />
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className='flex justify-evenly'>
+                <div className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+
+        {allData.map((data) => (
+            <div key={data.id}>
+                    <div className="ecommerce_border_color rounded-xl lg-shadow w-full h-full flex flex-col justify-between p-12">
+                      
+                            <img className="w-28 h-28 lg:w-32 lg:h-32 mx-auto" src="logo192.png" alt="product"></img>
+                            <h1 className="text-base md:text-xl mt-10">Bed Rooms: 2 | 2 Floors | 2810 Sqft House on Sale </h1>
+                            <p className="mobile-responsive-fontprice-product robotoFont font-bold text-base md:text-lg pt-4 price-color">1234 Maple Blossom, Emerald City, CA, 94785</p>
+                        
+                        </div>
+
+                    {/* <div class="card" style={{ width: '21rem' }}>
+                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp" class="card-img-top" alt="Sunset Over the Sea" />
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
-                </div>
-                <div class="card" style={{ width: '21rem' }}>
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp" class="card-img-top" alt="Sunset Over the Sea" />
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="card" style={{ width: '21rem' }}>
+                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp" class="card-img-top" alt="Sunset Over the Sea" />
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
                     </div>
-                </div>
-                <div class="card" style={{ width: '21rem' }}>
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp" class="card-img-top" alt="Sunset Over the Sea" />
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="card" style={{ width: '21rem' }}>
+                        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp" class="card-img-top" alt="Sunset Over the Sea" />
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div> */}
+
+                    
                     </div>
+                    ))}
                 </div>
             </div>
-
         </div>
     );
 }
